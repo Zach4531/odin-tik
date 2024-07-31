5ea3b0c7ebd6bc9fbafdb3b7f0387f63965b82ff
Zach4531 <zciatteo96@gmail.com>
2024-07-31 16:06:50 -0400

    Update game

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const elements = (function () {
   const cells = document.querySelectorAll('.cell');
-  return { cells };
+  const start = document.querySelector('.start');
+  const players = document.querySelectorAll('.players p');
+  const round = document.querySelector('.round');
+  return { cells, start, players, round };
 })();
 
 function playerFactory(name, marker) {
-  let points = 0;
+  let score = 0;
+  const scoreElement = document.querySelector(`.${name}-score`);
 
-  const getPoints = () => points;
-  const setPoints = () => points++;
-  const resetPoints = () => (points = 0);
+  const getScore = () => score;
+  const resetScore = () => (score = 0);
+  const setScore = () => {
+    score++;
+    scoreElement.textContent = score;
+  };
 
-  return { name, marker, getPoints, setPoints, resetPoints };
+  return { name, marker, getScore, setScore, resetScore };
 }
-
-const user1 = playerFactory('player1', 'X');
-const user2 = playerFactory('player2', 'O');
 
 const Gameboard = (function () {
   let gameboard = [null, null, null, null, null, null, null, null, null];
@@ -28,20 +32,97 @@ const Gameboard = (function () {
     [0, 3, 6],
     [2, 5, 8],
   ];
+
+  function setGameboard(index) {
+    gameboard[index] = Game.getPlayer().marker;
+    checkWinner();
+  }
+
+  function checkWinner() {
+    for (let i = 0; i < winPatterns.length; i++) {
+      const [a, b, c] = winPatterns[i];
+      if (
+        gameboard[a] == Game.getPlayer().marker &&
+        gameboard[b] == Game.getPlayer().marker &&
+        gameboard[c] == Game.getPlayer().marker
+      ) {
+        Game.getPlayer().setScore();
+        Game.setGameInctive();
+      }
+    }
+  }
+
+  function resetGame() {
+    gameboard = [null, null, null, null, null, null, null, null, null];
+    elements.cells.forEach((cell) => {
+      cell.textContent = '';
+    });
+  }
+
+  return { resetGame, setGameboard };
+})();
+
+const Game = (function () {
+  let round = 0;
   const players = [
     playerFactory('player1', 'X'),
     playerFactory('player2', 'O'),
   ];
 
-  function resetGame() {
-    gameboard = [null, null, null, null, null, null, null, null, null];
+  let currentPlayer = players[0];
+  let isGameActive = false;
+
+  elements.start.addEventListener('click', startGame);
+  elements.round.textContent = round;
+
+  function switchPlayer() {
+    currentPlayer = currentPlayer == players[0] ? players[1] : players[0];
+    if (currentPlayer == players[0]) {
+      elements.players[0].classList.add('active');
+      elements.players[1].classList.remove('active');
+    } else {
+      elements.players[0].classList.remove('active');
+      elements.players[1].classList.add('active');
+    }
   }
 
-  return { resetGame, getGameboard };
-})();
+  function handleClick({ currentTarget }) {
+    if (currentTarget.textContent == '' && getGameActive()) {
+      currentTarget.textContent = currentPlayer.marker;
+      Gameboard.setGameboard(currentTarget.dataset.cell);
+      switchPlayer();
+    }
+  }
 
-const Game = (function () {
+  const getPlayer = () => currentPlayer;
+  const getGameActive = () => isGameActive;
+  const setGameInctive = () => (isGameActive = false);
+  const setGameActive = () => (isGameActive = true);
+
+  const increaseRound = () => {
+    round++;
+    elements.round.textContent = round;
+  };
+
+  elements.cells.forEach((cell) => {
+    cell.addEventListener('click', handleClick);
+  });
+
   function startGame() {
+    increaseRound();
+    elements.players[0].classList.add('active');
+    elements.players[1].classList.remove('active');
+
+    setGameActive();
+    currentPlayer = players[0];
     Gameboard.resetGame();
   }
+
+  return {
+    startGame,
+    getPlayer,
+    setGameActive,
+    setGameInctive,
+    getGameActive,
+  };
 })();
